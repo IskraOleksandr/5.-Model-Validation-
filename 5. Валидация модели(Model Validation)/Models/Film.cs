8f113992a4aca2b962a8b36be92744caa1dd3aa3
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-01 15:40:24 +0300

    #Task3 модификации модели и Views Create, Edit

--- 5. Валидация модели(Model Validation)/Models/Film.cs
+++ 5. Валидация модели(Model Validation)/Models/Film.cs
@@ -8,30 +8,30 @@ namespace _5._Валидация_модели_Model_Validation_.Models
 
 		[Required(ErrorMessage = "Поле должно быть установлено.")]
 		[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 50 символов")]
-		[Display(Name = "Название")]
+		[Display(Name = "Название фильма: ")]
 		public string Title { get; set; }
 
 		[Required(ErrorMessage = "Поле должно быть установлено.")]
-		[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 50 символов")]
-		[Display(Name = "Режиссер")]
+		[StringLength(80, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 50 символов")]
+		[Display(Name = "Режиссер: ")]
 		public string Director { get; set; }
 
 		[Required(ErrorMessage = "Поле должно быть установлено.")]
-		[StringLength(50, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 3 до 70 символов")]
-		[Display(Name = "Жанр")]
+		[StringLength(80, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 3 до 70 символов")]
+		[Display(Name = "Жанр:")]
 		public string Genre { get; set; }
 
 		[Required(ErrorMessage = "Поле должно быть установлено.")]
 		[Range(1900, 2023)]
-		[Display(Name = "Год выпуска")]
+		[Display(Name = "Год выпуска: ")]
 		public int Year { get; set; }
 
-
-		public string? PosterPath { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        public string? PosterPath { get; set; }
 
 		[Required(ErrorMessage = "Поле должно быть установлено.")]
-		[StringLength(400, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 400 символов")]
-		[Display(Name = "Описа")]
+		[StringLength(1000, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 400 символов")]
+		[Display(Name = "Описание фильма: ")]
 		public string Description { get; set; }
 	}
 }
